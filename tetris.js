80ffea0cc1709f070d667a1b0d631a69e5260ba2
ferb96 <fduy96@gmail.com>
2017-10-19 14:26:57 -0500

    revamped score counting

--- tetris.js
+++ tetris.js
@@ -114,6 +114,8 @@ var gvar = {
 	level: 1,
 	// current score
 	score: 0,
+	// score multiplier
+	scoreMulti: 1,
 	// back-to-back clearing should award more points
 	justScored: false,
 	// is this the first tile ever?
@@ -473,14 +475,21 @@ function clearFull(){
 }
 
 function updateLevel(){
-	gvar.level = Math.floor(gvar.score / 3000) + 1;
-	if (gvar.level == 1) gvar.diffTimer = 1500;
-	else if (gvar.level == 2) gvar.diffTimer = 1200;
-	else if (gvar.level == 3) gvar.diffTimer = 900;
-	else if (gvar.level == 4) gvar.diffTimer = 750;
-	else if (gvar.level == 5) gvar.diffTimer = 650;
-	else if (gvar.level == 6) gvar.diffTimer = 575;
-	else gvar.diffTimer = 500;
+	if (gvar.score < 2000){ gvar.level=1; gvar.scoreMulti=1; gvar.diffTimer=1500; }
+	else if (gvar.score < 4000){ gvar.level=2; gvar.scoreMulti=1; gvar.diffTimer=1000; }
+	else if (gvar.score < 7000){ gvar.level=3; gvar.scoreMulti=2; gvar.diffTimer=750; }
+	else if (gvar.score < 10000){ gvar.level=4; gvar.scoreMulti=2; gvar.diffTimer=650; }
+	else if (gvar.score < 14000){ gvar.level=5; gvar.scoreMulti=3; gvar.diffTimer=600; }
+	else if (gvar.score < 18000){ gvar.level=6; gvar.scoreMulti=3; gvar.diffTimer=550; }
+	else if (gvar.score < 23000){ gvar.level=7; gvar.scoreMulti=4; gvar.diffTimer=500; }
+	else if (gvar.score < 28000){ gvar.level=8; gvar.scoreMulti=4; gvar.diffTimer=450; }
+	else if (gvar.score < 34000){ gvar.level=9; gvar.scoreMulti=5; gvar.diffTimer=400; }
+	else if (gvar.score < 40000){ gvar.level=10; gvar.scoreMulti=5; gvar.diffTimer=350; }
+	else {
+		gvar.level = Math.floor((gvar.score-40000)/7000);
+		gvar.scoreMulti = 6;
+		gvar.diffTimer = 300;
+	}
 }
 
 function commit(){
@@ -492,7 +501,7 @@ function commit(){
 		aTile.y += 1;
 		count += 1;
 	}
-	gvar.score+=count;
+	gvar.score+=count*gvar.scoreMulti;
 	// now we create new textures in the same place
 	for (var i=0; i<aTile.sq.length; i++){
 		var newx = aTile.x + aTile.sc[i][0];
@@ -514,11 +523,11 @@ function commit(){
 	// remove any row if it is full
 	if (del.length>0){
 		// reward the player with points for clearing rows
-		if (del.length == 1) gvar.score += 100;
-		if (del.length == 2) gvar.score += 300;
-		if (del.length == 3) gvar.score += 500;
-		if (del.length == 4) gvar.score += 800;
-		if (gvar.justScored) gvar.score += 250;
+		if (del.length == 1) gvar.score += 100*gvar.scoreMulti;
+		if (del.length == 2) gvar.score += 300*gvar.scoreMulti;
+		if (del.length == 3) gvar.score += 500*gvar.scoreMulti;
+		if (del.length == 4) gvar.score += 800*gvar.scoreMulti;
+		if (gvar.justScored) gvar.score += 250*gvar.scoreMulti;
 		gvar.justScored = true;
 		// update the level
 		updateLevel();
@@ -698,10 +707,10 @@ function getRandomType(){
 		stats[result]+=1;
 	} else {
 		// construct an array
-		var chance = [ITILE,ITILE,JTILE,JTILE,LTILE,LTILE];
-		for (var i=0; i<7; i++)
-			for (var j=0; j< (max-stats[i]+1)+Math.floor((max-stats[i])*0.75); j++) chance.push(i);
-		shuffle(chance)
+		var chance = [ITILE,ITILE,ITILE,JTILE,JTILE,LTILE,LTILE,OTILE,STILE,ZTILE,TTILE,TTILE];
+		//for (var i=0; i<7; i++)
+		//	for (var j=0; j< (max-stats[i]+1)+Math.floor((max-stats[i])*0.75); j++) chance.push(i);
+		shuffle(chance);
 		result = chance[Math.floor(Math.random() * chance.length)];
 		stats[result]+=1;
 	}
